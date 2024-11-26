a67f035015ce5f9a58d4069e0c05b85b95c857ab
ccs96307 <ccs96307@gmail.com>
2024-11-26 22:02:05 +0800

    [Update] Kangaroo Training

--- kangaroo/kangaroo_modeling/modeling_kangaroo_llama3.py
+++ kangaroo/kangaroo_modeling/modeling_kangaroo_llama3.py
@@ -11,16 +11,17 @@ from transformers.modeling_outputs import CausalLMOutputWithPast
 @dataclass
 class KangarooModelMode:
     draft_only_mode: str = "draft_only"
-    target_only_model: str = "target_only"
+    target_only_mode: str = "target_only"
     accelerate_mode: str = "accelerate"
+    train_mode: str = "train"
 
 
-class KangarooGemma2ForCausalLM(LlamaForCausalLM):
+class KangarooLlamaForCausalLM(LlamaForCausalLM):
     def __init__(self, config):
         super().__init__(config)
-        self.draft_mode_adapter_layer = LlamaSdpaAttention()(config=config, layer_idx=len(config.num_hidden_layers))
+        self.draft_mode_adapter_layer = LlamaSdpaAttention(config=config, layer_idx=config.num_hidden_layers)
         self.shallow_layers = None
-        self.mode = KangarooModelMode.target_only_model
+        self.mode = KangarooModelMode.target_only_mode
     
     def set_skip_layer(self, shallow_layer_num: int) -> None:
         self.shallow_layers = self.model.layers[:shallow_layer_num]
@@ -30,7 +31,10 @@ class KangarooGemma2ForCausalLM(LlamaForCausalLM):
         self.mode = KangarooModelMode.draft_only_mode
 
     def set_target_mode(self) -> None:
-        self.mode = KangarooModelMode.target_only_model
+        self.mode = KangarooModelMode.target_only_mode
+
+    def set_train_mode(self) -> None:
+        self.mode = KangarooModelMode.train_mode
 
     def set_acceleration_mode(self) -> None:
         self.mode = KangarooModelMode.accelerate_mode
@@ -51,8 +55,8 @@ class KangarooGemma2ForCausalLM(LlamaForCausalLM):
         num_logits_to_keep: int = 0,
         **loss_kwargs,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
-        if self.mode == KangarooModelMode.target_only_model:
-            return super(
+        if self.mode == KangarooModelMode.target_only_mode:
+            return super.forward(
                 input_ids=input_ids,
                 attention_mask=attention_mask,
                 position_ids=position_ids,
@@ -74,8 +78,122 @@ class KangarooGemma2ForCausalLM(LlamaForCausalLM):
             )
             return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
-
             ### === MODEL ===
+            if (input_ids is None) ^ (inputs_embeds is not None):
+                raise ValueError("You must specify exactly one of input_ids or inputs_embeds")
+
+            if self.model.gradient_checkpointing and self.model.training and use_cache:
+                use_cache = False
+
+            if inputs_embeds is None:
+                inputs_embeds = self.model.embed_tokens(input_ids)
+
+            # kept for BC (non `Cache` `past_key_values` inputs)
+            return_legacy_cache = False
+            if use_cache and not isinstance(past_key_values, Cache):
+                return_legacy_cache = True
+                if past_key_values is None:
+                    past_key_values = DynamicCache()
+                else:
+                    past_key_values = DynamicCache.from_legacy_cache(past_key_values)
+
+            if cache_position is None:
+                past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
+                cache_position = torch.arange(
+                    past_seen_tokens, past_seen_tokens + inputs_embeds.shape[1], device=inputs_embeds.device
+                )
+            if position_ids is None:
+                position_ids = cache_position.unsqueeze(0)
+
+            print(attention_mask.shape)
+            causal_mask = self.model._update_causal_mask(
+                attention_mask, inputs_embeds, cache_position, past_key_values, output_attentions
+            )
+            hidden_states = inputs_embeds
+
+            # create position embeddings to be shared across the decoder layers
+            position_embeddings = self.model.rotary_emb(hidden_states, position_ids)
+
+            # decoder layers
+            all_hidden_states = () if output_hidden_states else None
+            all_self_attns = () if output_attentions else None
+            next_decoder_cache = None
+
+            for decoder_layer in self.shallow_layers:
+                if output_hidden_states:
+                    all_hidden_states += (hidden_states,)
+
+                layer_outputs = decoder_layer(
+                    hidden_states,
+                    attention_mask=causal_mask,
+                    position_ids=position_ids,
+                    past_key_value=past_key_values,
+                    output_attentions=output_attentions,
+                    use_cache=use_cache,
+                    cache_position=cache_position,
+                    position_embeddings=position_embeddings,
+                )
+
+                hidden_states = layer_outputs[0]
+
+                if use_cache:
+                    next_decoder_cache = layer_outputs[2 if output_attentions else 1]
+
+                if output_attentions:
+                    all_self_attns += (layer_outputs[1],)
+
+            ### Adapter
+            residual = hidden_states
+            hidden_states = self.model.norm(hidden_states)
+
+            # add hidden states from the last decoder layer
+            if output_hidden_states:
+                all_hidden_states += (hidden_states,)
+
+            next_cache = next_decoder_cache if use_cache else None
+            if return_legacy_cache:
+                next_cache = next_cache.to_legacy_cache()
+
+            hidden_states, self_attn_weights, past_key_values = self.draft_mode_adapter_layer(
+                hidden_states=hidden_states,
+                attention_mask=attention_mask,
+                position_ids=position_ids,
+                past_key_values=past_key_values,
+                output_attentions=output_attentions,
+                use_cache=use_cache,
+                cache_position=cache_position,
+                position_embeddings=position_embeddings,
+            )
+
+            hidden_states = residual + hidden_states
+            hidden_states = self.model.norm(hidden_states)
+
+            if self.config.pretraining_tp > 1:
+                lm_head_slices = self.lm_head.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
+                logits = [torch.nn.functional.linear(hidden_states, lm_head_slices[i]) for i in range(self.config.pretraining_tp)]
+                logits = torch.cat(logits, dim=-1)
+            else:
+                # Only compute necessary logits, and do not upcast them to float if we are not computing the loss
+                logits = self.lm_head(hidden_states[:, -num_logits_to_keep:, :])
+
+            loss = None
+            if labels is not None:
+                loss = self.loss_function(logits=logits, labels=labels, vocab_size=self.config.vocab_size, **loss_kwargs)
+
+            return CausalLMOutputWithPast(
+                loss=loss,
+                logits=logits,
+                past_key_values=past_key_values,
+                hidden_states=hidden_states,
+                attentions=self_attn_weights,
+            )
+        elif self.mode == KangarooModelMode.train_mode:
+            output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+            output_hidden_states = (
+                output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+            )
+            return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
             if (input_ids is None) ^ (inputs_embeds is not None):
                 raise ValueError("You must specify exactly one of input_ids or inputs_embeds")
 
@@ -138,11 +256,14 @@ class KangarooGemma2ForCausalLM(LlamaForCausalLM):
                 if output_attentions:
                     all_self_attns += (layer_outputs[1],)
 
-            ### Adapter
-            residual = hidden_states
-            hidden_states = self.norm(hidden_states)
+            # Cache hidden states
+            remaining_hidden_states = hidden_states
 
-            # add hidden states from the last decoder layer
+            # Attention adapter
+            residual = hidden_states
+            hidden_states = self.model.norm(hidden_states)
+
+            # Add hidden states from the last decoder layer
             if output_hidden_states:
                 all_hidden_states += (hidden_states,)
 
@@ -152,7 +273,7 @@ class KangarooGemma2ForCausalLM(LlamaForCausalLM):
 
             hidden_states, self_attn_weights, past_key_values = self.draft_mode_adapter_layer(
                 hidden_states=hidden_states,
-                attention_mask=attention_mask,
+                attention_mask=causal_mask,
                 position_ids=position_ids,
                 past_key_values=past_key_values,
                 output_attentions=output_attentions,
@@ -162,23 +283,48 @@ class KangarooGemma2ForCausalLM(LlamaForCausalLM):
             )
 
             hidden_states = residual + hidden_states
-            hidden_states = self.norm(hidden_states)
+            hidden_states = self.model.norm(hidden_states)
 
-            if self.config.pretraining_tp > 1:
-                lm_head_slices = self.lm_head.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
-                logits = [torch.nn.functional.linear(hidden_states, lm_head_slices[i]) for i in range(self.config.pretraining_tp)]
-                logits = torch.cat(logits, dim=-1)
-            else:
-                # Only compute necessary logits, and do not upcast them to float if we are not computing the loss
-                logits = self.lm_head(hidden_states[:, -num_logits_to_keep:, :])
+            # Remaining decoder layers
+            for decoder_layer in self.remaining_layers:
+                if output_hidden_states:
+                    all_hidden_states += (hidden_states,)
 
-            loss = None
-            if labels is not None:
-                loss = self.loss_function(logits=logits, labels=labels, vocab_size=self.config.vocab_size, **loss_kwargs)
+                layer_outputs = decoder_layer(
+                    remaining_hidden_states,
+                    attention_mask=causal_mask,
+                    position_ids=position_ids,
+                    past_key_value=past_key_values,
+                    output_attentions=output_attentions,
+                    use_cache=use_cache,
+                    cache_position=cache_position,
+                    position_embeddings=position_embeddings,
+                )
+
+                remaining_hidden_states = layer_outputs[0]
+
+                if use_cache:
+                    next_decoder_cache = layer_outputs[2 if output_attentions else 1]
+
+                if output_attentions:
+                    all_self_attns += (layer_outputs[1],)
+
+            remaining_hidden_states = self.model.norm(remaining_hidden_states)
+
+            # Only compute necessary logits, and do not upcast them to float if we are not computing the loss
+            draft_logits = self.lm_head(hidden_states[:, -num_logits_to_keep:, :])
+            target_logits = self.lm_head(remaining_hidden_states[:, -num_logits_to_keep:, :])
+
+            # Calculate the log-probabilities of target model and draft model
+            target_log_probs = torch.nn.functional.log_softmax(target_logits, dim=-1)
+            draft_probs = torch.nn.functional.softmax(draft_logits, dim=-1)
+
+            # Cross-entropy loss between target and draft model predictions
+            loss = -(draft_probs * target_log_probs).sum(dim=-1).mean()
 
             return CausalLMOutputWithPast(
                 loss=loss,
-                logits=logits,
+                logits=target_logits,
                 past_key_values=past_key_values,
                 hidden_states=hidden_states,
                 attentions=self_attn_weights,
---
+++ kangaroo/train.py
@@ -0,0 +1,134 @@
+from typing import Dict, List
+
+from datasets import load_dataset
+
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from kangaroo_modeling.modeling_kangaroo_llama3 import KangarooLlamaForCausalLM
+
+
+class CustomDataset(Dataset):
+    def __init__(self, inputs: Dict[str, torch.LongTensor], device: torch.DeviceObjType):
+        self.inputs = inputs
+        self.device = device
+
+    def __len__(self) -> int:
+        return self.inputs.input_ids.shape[0]
+    
+    def __getitem__(self, index: int):
+        return (
+            self.inputs.input_ids[index].to(self.device),
+            self.inputs.attention_mask[index].to(self.device),
+        )
+    
+
+def main() -> None:
+    # Settings
+    epochs = 5
+    batch_size = 4
+    lr = 1e-5
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # Load model and tokenizer
+    pretrained_model_name_or_path = "../models/HuggingFaceTB--SmolLM2-135M-Instruct"
+    model = KangarooLlamaForCausalLM.from_pretrained(pretrained_model_name_or_path).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path)
+
+    model.set_skip_layer(shallow_layer_num=10)
+    model.set_train_mode()
+
+    # Freeze all parameters
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # Unfreeze adapter layer
+    model.draft_mode_adapter_layer.train()
+    for param in model.draft_mode_adapter_layer.parameters():
+        param.requires_grad = True
+
+    # Load dataset
+    dataset = load_dataset("shibing624/sharegpt_gpt4")
+    
+    train_samples = dataset["train"]["conversations"][:4000]
+    train_samples = [[{"role": sample[0]["from"].replace("human", "user").replace("gpt", "assistant"), "content": sample[0]["value"]}] for sample in train_samples]
+
+    eval_samples = dataset["train"]["conversations"][4000:4500]
+    eval_samples = [[{"role": sample[0]["from"].replace("human", "user").replace("gpt", "assistant"), "content": sample[0]["value"]}] for sample in eval_samples]
+
+    # Tokenized
+    train_inputs = tokenizer(
+        [tokenizer.apply_chat_template(messages, tokenize=False) for messages in train_samples],
+        return_tensors="pt",
+        padding=True,
+        truncation=True,
+        max_length=512,
+    ).to(device)    
+
+    eval_inputs = tokenizer(
+        [tokenizer.apply_chat_template(messages, tokenize=False) for messages in eval_samples],
+        return_tensors="pt",
+        padding=True,
+        truncation=True,
+        max_length=512,
+    ).to(device)    
+
+    train_dataset = CustomDataset(inputs=train_inputs, device=device)
+    eval_dataset = CustomDataset(inputs=eval_inputs, device=device)
+
+    # Dataloader
+    train_dataloader = DataLoader(train_dataset)
+    eval_dataloader = DataLoader(eval_dataset)
+
+    # Optimizer
+    optimizer = torch.optim.AdamW(model.draft_mode_adapter_layer.parameters(), lr=lr)
+
+    # Training loop
+    for epoch in range(epochs):
+        model.train()
+        total_loss = 0
+
+        for batch_idx, batch in enumerate(train_dataloader, 1):
+            input_ids, attention_mask = batch
+
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+
+            # Zero gradients
+            optimizer.zero_grad()
+
+            # Calculate loss
+            loss = outputs.loss
+            total_loss += loss.item()
+
+            # Backward pass
+            loss.backward()
+
+            # Optimizer step
+            optimizer.step()
+
+            # Log training loss
+            avg_loss = total_loss / batch_idx
+            print(f"Train - Epoch [{epoch + 1}/{epochs}] Steps [{batch_idx}/{len(train_dataloader)}], Training Loss: {avg_loss:.4f}")
+
+        # Evaluate the model
+        model.eval()
+        eval_loss = 0
+        with torch.no_grad():
+            for batch_idx, batch in enumerate(eval_dataloader, 1):
+                input_ids, attention_mask = batch
+
+                outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+                eval_loss += outputs.loss.item()
+
+                avg_loss = eval_loss / batch_idx
+                print(f"Eval - Epoch [{epoch + 1}/{epochs}] Steps [{batch_idx}/{len(train_dataloader)}], Training Loss: {avg_loss:.4f}")
+
+        # Save model checkpoint
+        checkpoint_path = f"./checkpoints/kangaroo_model_epoch_{epoch + 1}.pt"
+        torch.save(model.state_dict(), checkpoint_path)
+        print(f"Model checkpoint saved at {checkpoint_path}")
+
+
+if __name__ == "__main__":
+    main()
